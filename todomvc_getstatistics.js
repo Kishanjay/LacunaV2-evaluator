001748d7b1522c73999f4c354bd117b6bc5470c5
Kishan Nirghin <kishanjay@live.nl>
2019-04-09 17:07:25 +0200

    Updates statistics (simplifies a lot)

--- todomvc_getstatistics.js
+++ todomvc_getstatistics.js
@@ -45,7 +45,7 @@ var stats = {
 };
 
 /* Fetch all and alive functions from the frameworks and export to csv */
-var csvData = "Framework,Analyzer,AllFunctions,AliveFunctions,DeadFunctions,TrueDeadFunctions,TrueAliveFunctions\n";
+var csvData = "Framework,Analyzer,AllFunctions,DeadFunctions,ClaimedDeadFunctions,TrueDeadFunctions,AliveFunctions,ClaimedAliveFunctions,TrueAliveFunctions\n";
 frameworks.forEach((framework) => {
     var aliveFunctionsPath = path.join("todomvc", "examples", framework.name, ALIVE_FUNCTIONS_FILE);
     var allFunctionsPath = path.join("todomvc", framework.path, ALL_FUNCTIONS_FILE);
@@ -57,8 +57,6 @@ frameworks.forEach((framework) => {
     numberOfAliveFunctions = aliveFunctions.length;
     var numberOfFunctions = allFunctions.length;
     var numberOfDeadFunctions = numberOfFunctions - numberOfAliveFunctions;
-
-    csvData += `${framework.name},<groundtruth>,${numberOfFunctions},${numberOfAliveFunctions},${numberOfDeadFunctions},${numberOfDeadFunctions}/${numberOfDeadFunctions},${numberOfAliveFunctions}/${numberOfAliveFunctions}\n`;
 
     ANALYZERS.forEach(analyzer => {
         try {
@@ -75,58 +73,12 @@ frameworks.forEach((framework) => {
             var analyzerNumberOfFalseDeadFunctions = analyzerDeadFunctions.length - analyzerNumberOfTrueDeadFunctions;
             var analyzerNumberOfTrueAliveFunctions = numberOfAliveFunctions - analyzerNumberOfFalseDeadFunctions;
 
-            csvData += `${framework.name},${AnalyzerMap[analyzer]},${analyzerAllFunctions.length},${analyzerAliveFunctions.length},${analyzerDeadFunctions.length},${analyzerNumberOfTrueDeadFunctions}/${analyzerDeadFunctions.length}(P) ${analyzerNumberOfTrueDeadFunctions}/${numberOfDeadFunctions}(R),${analyzerNumberOfTrueAliveFunctions}/${numberOfAliveFunctions} (-${analyzerNumberOfFalseDeadFunctions})\n`;
-
-            /**
-             * Store the precision, recall, and fscore of this framework into 
-             * the array.
-             */
-            var precision = analyzerNumberOfTrueDeadFunctions / analyzerDeadFunctions.length;
-            var recall = analyzerNumberOfTrueDeadFunctions / numberOfDeadFunctions;
-            var fscore = 2 * ((precision * recall) / (precision + recall));
-
-            stats[AnalyzerMap[analyzer]].precisions.push(precision);
-            stats[AnalyzerMap[analyzer]].recalls.push(recall);
-            stats[AnalyzerMap[analyzer]].fscores.push(fscore);
-            
+            csvData += `${framework.name},${AnalyzerMap[analyzer]},${analyzerAllFunctions.length},${numberOfDeadFunctions},${analyzerDeadFunctions.length},${analyzerNumberOfTrueDeadFunctions},${numberOfAliveFunctions},${analyzerAliveFunctions.length},${analyzerNumberOfTrueAliveFunctions}\n`    
         } catch (e) { console.log(e); }
     });
 });
 
 fs.writeFileSync(path.join(__dirname, STATISTICS_FILE), csvData, 'utf8');
-
-
-/**
- * A bit of a sloppy piece to calculate the precision, recall and f-scores..
- * at first it computes the precision, recall and f-score for every framework
- * and stores that value into an array. Followed by a part where calculates
- * the averages of all.
- * 
- * The add function is a helper function for the reduce func.
- */
-function add(x, y) { return x + y; }
-var mean = {
-    static: { precision: null, recall: null, fscore: null },
-    dynamic: { precision: null, recall: null, fscore: null },
-    hybrid: { precision: null, recall: null, fscore: null }
-};
-mean.static.precision = stats.static.precisions.reduce(add) / stats.static.precisions.length;
-mean.static.recall = stats.static.recalls.reduce(add) / stats.static.recalls.length;
-mean.static.fscore = stats.static.fscores.reduce(add) / stats.static.fscores.length;
-mean.dynamic.precision = stats.dynamic.precisions.reduce(add) / stats.dynamic.precisions.length;
-mean.dynamic.recall = stats.dynamic.recalls.reduce(add) / stats.dynamic.recalls.length;
-mean.dynamic.fscore = stats.dynamic.fscores.reduce(add) / stats.dynamic.fscores.length;
-mean.hybrid.precision = stats.hybrid.precisions.reduce(add) / stats.hybrid.precisions.length;
-mean.hybrid.recall = stats.hybrid.recalls.reduce(add) / stats.hybrid.recalls.length;
-mean.hybrid.fscore = stats.hybrid.fscores.reduce(add) / stats.hybrid.fscores.length;
-
-var data = linearExport(stats.static.precisions, stats.static.recalls, stats.dynamic.precisions, stats.dynamic.recalls, stats.hybrid.precisions, stats.hybrid.recalls);
-data = `staticPrecision \t staticRecall \t dynamicPrecision \t dynamicRecall \t hybridPrecision \t hybridRecall \n` + data; 
-fs.writeFileSync(path.join(__dirname, "precision_recall.data"), data, 'utf8');
-
-console.log(mean);
-
-
 
 
 /* Helper functions */
@@ -135,35 +87,8 @@ function loadJSONFile(functionsFilePath) {
 }
 
 /**
- * Helper function that exports data in columns. e.g. passing x arrays of length
- * y will export every y values of x[i] on the ith column.
- * 
- * Making it readable by R with the read.table and fetching x[i] with rx[,i]
+ * The normalize function should be removed.
  */
-function linearExport() {
-    var lExport = "";
-    /* error checking */
-    var columnLength = arguments[0].length;
-    for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i].length != columnLength) {
-            console.log("Warning invalid columnLength: " + arguments[i].length);
-        }
-    }
-    /* export lines */
-    for (var i = 0; i < columnLength; i++) {
-        var line = "";
-        for (var j = 0; j < arguments.length; j++) {
-            line += arguments[j][i] + '\t';
-        }
-        lExport += line + '\n';
-    }
-    
-    return lExport;
-}
-
-
-
-
 function countTrueDeadFunctions(claimedDeadFunctions, aliveFunctions) {
     var counter = claimedDeadFunctions.length;
     claimedDeadFunctions.forEach((claimedDeadFunction) => {
