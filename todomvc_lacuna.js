ddcec71ea1fd4ab4ffb47482dc3cc8a8fd86ce67
Kishan Nirghin <kishanjay@live.nl>
2019-05-30 00:22:54 +0200

    Updates lacuna runner for todomvc

--- todomvc_lacuna.js
+++ todomvc_lacuna.js
@@ -8,51 +8,159 @@ const path = require("path");
 const commandLineArgs = require("command-line-args");
 const lacuna = require("../LacunaV2/lacuna_runner");
 
-/* Fix relative path issue */
-const cwd = process.cwd();
-process.chdir('./todomvc/tests');
-var frameworkPathLookup = require('./todomvc/tests/framework-path-lookup');
-var frameworks = frameworkPathLookup();
-process.chdir(cwd);
 
-/* The (combination of) analysers that lacuna will use on the todomvc */
-const ANALYZERS = ["static nativecalls", "dynamic", "static nativecalls dynamic"];
+const TODOMVC_DIR = "todomvc";
+const EXAMPLES_DIR = "examples.normalized";
 
-try {
-    /* Running in chunks to prevent the heap out of memory error */
-    let options = {
-        offset: 0,
-        length: 10,
-        simulate: false,
-    };
-    let argv = commandLineArgs([
-        { name: 'offset', type: Number, alias: 'o' },
-        { name: 'length', type: Number, alias: 'l' },
-        /* Debug arg: only show what commands are getting executed */
-        { name: 'simulate', type: Boolean, alias: 's' }, 
-    ]);
-    options.extend(argv);
+const ANALYZERS = ["static", "nativecalls", "dynamic", "closure_compiler", "wala", "npm_cg", "tajs", "acg"];
 
-    start(options).then(() => { console.log("done"); });
-} catch (e) { console.log(e);}
+start().then((result) => {
+    console.log("Finished");
+}).catch((e) => {
+    console.log(e);
+    console.log("catch error");
+}).finally(() => {
+    console.log("finally");
+})
 
+async function start() {
+    let options = getRunOptions();
+    assert(options, "Invalid options");
 
+    let frameworks = getFrameworks(options);
+    assert(frameworks, "Invalid frameworks");
 
+    let analyserCombinations = generateAnalyserCombinations(ANALYZERS);
+    assert(analyserCombinations, "Invalid analyserCombinations");
 
-/**
- * Function to run lacuna synchronously 
- *  (Required for dynamic analyzer to work)
- */
-async function start(options) {
-    var runOptions = generateRunOptions(options.offset, options.length);
-    if (options.simulate) { console.log(runOptions); process.exit(); }
+    let lacunaRunOptions = generateLacunaRunOptions(frameworks, analyserCombinations);
+    assert(lacunaRunOptions, "Invalid lacunaRunOptions");
+
+    if (options.simulate) { console.log(lacunaRunOptions); process.exit(); }
     
-    for (var i = 0; i < runOptions.length; i++) {
-        console.log(`\n\nLacuna_runner [${i}/${runOptions.length-1}] FROM [${options.offset}-${options.length}]`);
-        await runLacuna(runOptions[i]); // remove await for async **shocker**
+    /* Before running, normalize all frameworks according to Lacuna */
+    lacunaNormalizeFrameworks(frameworks);
+    return
+
+    for (let lacunaRunOption of lacunaRunOptions) {
+        
+        let containsDynamic = lacunaRunOption.analyzer.includes("dynamic");
+        
+        if (options.skipDynamic && containsDynamic) continue;
+        if (options.onlyDynamic && !containsDynamic) continue;
+
+        await runLacuna(lacunaRunOption); // remove await for async **shocker**
+        // NOTE: should wait for the dynamic analyser to function properly..
     }
 }
 
+
+/**
+ * Gets the runOptions of this program 
+ * combines the defaultOptions with the commandLineOptions
+ */
+function getRunOptions() {
+    let defaultOptions = {
+        offset: 0,
+        length: 10,
+        simulate: false,
+        skipDynamic: false,
+        onlyDynamic: false
+    };
+    try {
+        let argv = commandLineArgs([
+            { name: 'offset', type: Number, alias: 'o' },
+            { name: 'length', type: Number, alias: 'l' },   
+            { name: 'simulate', type: Boolean }, // DEBUG only log the runOptions (no execution)
+            { name: 'skipDynamic', type: Boolean }, // Skips run that use dynamic analyser
+            { name: 'onlyDynamic', type: Boolean} // Only do runs that use dynamic analyser
+        ]);
+        defaultOptions.extend(argv);
+    } catch (e) { throw new Error(e); }
+    finally {
+        return defaultOptions;
+    }
+}
+
+
+/**
+ * Fetches the todomvc- frameworks that will be used
+ */
+function getFrameworks({ offset, length }) {
+    const tests_dir = path.join(TODOMVC_DIR, "tests")
+    const cwd = process.cwd(); // fixes relative path issue
+    process.chdir(tests_dir);
+    const frameworkPathLookup = require("./" + path.join(tests_dir, 'framework-path-lookup'));
+    const frameworks = frameworkPathLookup().slice(offset, offset + length);
+    process.chdir(cwd);
+
+    return frameworks;
+}
+
+/**
+ * Will create all combinations of analysers as a space seperated string.
+ * 
+ * @param {*} analysers array of analysers that will be considered for the
+ * combinations.
+ */
+function generateAnalyserCombinations(analysers) {
+    let result = [];
+    let f = function(prefix, items) {
+        for (let i = 0; i < items.length; i++) {
+            let analyserCombination = (prefix + " " + items[i]).trim();
+            result.push(analyserCombination);
+            f(analyserCombination, items.slice(i + 1));
+        }
+    }
+    f('', analysers);
+    return result;
+}
+
+/**
+ * Generates all runOptions of Lacuna.
+ * For now that means to gather runoptions for every analyzer
+ * for every framework
+ */
+function generateLacunaRunOptions(frameworks, analyserCombinations) {
+    var lacunaRunOptions = [];
+
+    frameworks.forEach(framework => {
+        let directory = generateFrameworkDirectory(framework);
+        analyserCombinations.forEach((analyserCombination) => {
+            /* Create new object for every setup */
+            let logfile = "lacuna_" + analyserCombination.replace(/ /gi, "") + ".log";
+            let lacunaRunOption = { 
+                directory: directory,
+                analyzer: analyserCombination.split(" "), // as an array,
+                logfile: logfile,
+                assumeNormalization: true, // should be normalized before starting
+            };
+            
+            lacunaRunOptions.push(lacunaRunOption);
+        });
+    });
+    return lacunaRunOptions;
+}
+
+/**
+ * Lacuna normalizes the frameworks 
+ * - exports inline JS
+ * - imports externally hosted JS
+ * - exports eventAttributes to a file
+ */
+function lacunaNormalizeFrameworks(frameworks) {
+    frameworks.forEach(framework => {
+        let directory = generateFrameworkDirectory(framework);
+        /* Create new object for every setup */
+        let normalizeRunOption = { 
+            directory: directory,
+            entry: "index.html",
+            normalizeOnly: true
+        };
+        
+        lacuna.run(normalizeRunOption);
+    });
+}
 /**
  * Lacuna promise required for synchronous execution
  */
@@ -64,47 +172,23 @@ function runLacuna(runOption) {
 }
 
 /**
- * Generates all runOptions of Lacuna.
- * For now that means to gather runoptions for every analyzer
- * for every framework
+ * Fetches the directory for a framework
  */
-function generateRunOptions(offset, length) {
-    currentFrameworks = frameworks.slice(offset, offset + length); // nifty lil thing to skip frameworks. / debug
-    var runOptions = [];
-    currentFrameworks.forEach(framework => {
-        let directory = generateFrameworkDirectory(framework);
-        ANALYZERS.forEach((analyzer) => {
-            /* Create new object for every setup */
-            let runOption = { 
-                directory: directory,
-                analyzer: null,
-                entry: "index.html",
-                destination: null,
-                logfile: null,
-                timeout: null,
-                olevel: 0,
-                force: false,
-            };
-            runOption.analyzer = analyzer.split(" ");
-            runOption.logfile = "lacuna_" + analyzer.replace(/ /gi, "") + ".log";
-            runOptions.push(runOption);
-        });
-    });
-    return runOptions;
+function generateFrameworkDirectory({path: frameworkPath}) {
+    frameworkPath = frameworkPath.splice(0, 8, EXAMPLES_DIR); // append to examples
+    process.exit();
+    
+    let pwdFrameworkPath = path.join(TODOMVC_DIR, frameworkPath);
+    return pwdFrameworkPath;
 }
 
-/**
- * Fetches the correct directory for a framework
- * Currently uses the backup directory since there is a chance that the
- * original todomvc is already overwritten with instrumentation code
- */
-function generateFrameworkDirectory(framework) {
-    var frameworkPath = framework.path;
-    frameworkPath = frameworkPath.splice(8, 0, ".back");
-    frameworkPath = path.join("todomvc", frameworkPath);
-    return frameworkPath;
-}
 
+function assert(assertion, msg) {
+    if (!assertion) {
+        console.log(msg);
+        process.exit(1);
+    }
+}
 
 
 function runDebug() {
